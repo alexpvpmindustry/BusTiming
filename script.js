8b7141dcad23b0790f749e1ddc553060a7e2bdf0
apmapmapm <alexpvpmindustry@github.com>
2024-07-23 19:41:18 +0800

    update ui

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function getLocation() {
     if (navigator.geolocation) {
-        document.getElementById("busstops").innerHTML = "fetching geo data.";
+        document.getElementById("busstops").innerHTML = "";
+        document.getElementById("loader").style.display = "block";
         navigator.geolocation.getCurrentPosition(fetchBusStops, showError);
-        document.getElementById("busstops").innerHTML = "Geolocation updated.";
     } else {
         document.getElementById("busstops").innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showError(error) {
+    document.getElementById("loader").style.display = "none";
     switch(error.code) {
         case error.PERMISSION_DENIED:
             document.getElementById("busstops").innerHTML = "User denied the request for Geolocation.";
@@ -32,6 +33,8 @@ async function fetchBusStops(position) {
     try {
         const response = await fetch('https://data.busrouter.sg/v1/stops.min.json');
         const busStops = await response.json();
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("busstops").innerHTML = "Geolocation updated.";
         const busStopsArray = Object.keys(busStops).map(key => ({
             code: key,
             name: busStops[key][2],
@@ -46,6 +49,7 @@ async function fetchBusStops(position) {
 
         displayBusStops(sortedBusStops.slice(0, 10));
     } catch (error) {
+        document.getElementById("loader").style.display = "none";
         document.getElementById("busstops").innerHTML = "Failed to fetch bus stops data.";
     }
 }
@@ -65,25 +69,24 @@ function displayBusStops(busStops) {
     const busStopsList = document.getElementById("busstops");
     busStopsList.innerHTML = "";
     busStops.forEach(stop => {
-        const li = document.createElement("button");
+        const li = document.createElement("li");
         li.textContent = `${stop.name} (${stop.code}, Dist: ${stop.distance.toFixed(2)} km)`;
         li.onclick = () => fetchBusTimings(stop.code);
         busStopsList.appendChild(li);
-        const div = document.createElement("div");
-        busStopsList.appendChild(div);
     });
 }
-let counter =0;
+
 async function fetchBusTimings(busStopId) {
     try {
+        document.getElementById("loader").style.display = "block";
         url = `https://arrivelah2.busrouter.sg/?id=${busStopId}`;
         const response = await fetch(url);
-        console.log(url);
         const data = await response.json();
+        document.getElementById("loader").style.display = "none";
         displayBusTimings(data);
     } catch (error) {
-        counter+=1;
-        document.getElementById("bustimings").innerHTML = "Failed to fetch bus timings."+counter;
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("bustimings").innerHTML = "Failed to fetch bus timings.";
     }
 }
 
@@ -98,22 +101,22 @@ function displayBusTimings(data) {
 
             if (service.next) {
                 const nextArrival = new Date(service.next.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next: ${nextArrival}, Load: ${service.next.load},${service.next.feature},${service.next.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next: ${nextArrival}, Load: ${service.next.load}, ${service.next.feature}, ${service.next.type}</p>`;
             }
 
             if (service.subsequent) {
                 const subsequentArrival = new Date(service.subsequent.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Subsequent: ${subsequentArrival}, Load: ${service.subsequent.load},${service.subsequent.feature},${service.subsequent.type}</p>`;
+                serviceDiv.innerHTML += `<p>Subsequent: ${subsequentArrival}, Load: ${service.subsequent.load}, ${service.subsequent.feature}, ${service.subsequent.type}</p>`;
             }
 
             if (service.next2) {
                 const next2Arrival = new Date(service.next2.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next 2: ${next2Arrival}, Load: ${service.next2.load},${service.next2.feature},${service.next2.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next 2: ${next2Arrival}, Load: ${service.next2.load}, ${service.next2.feature}, ${service.next2.type}</p>`;
             }
 
             if (service.next3) {
                 const next3Arrival = new Date(service.next3.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next 3: ${next3Arrival}, Load: ${service.next3.load},${service.next3.feature},${service.next3.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next 3: ${next3Arrival}, Load: ${service.next3.load}, ${service.next3.feature}, ${service.next3.type}</p>`;
             }
 
             busTimingsDiv.appendChild(serviceDiv);
