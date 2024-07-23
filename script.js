41cfef633348615e49c36864c8993f0e1e22df47
alex <67626131+alexpvpmindustry@users.noreply.github.com>
2024-07-23 18:08:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -96,22 +96,22 @@ function displayBusTimings(data) {
 
             if (service.next) {
                 const nextArrival = new Date(service.next.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next Arrival: ${nextArrival}, Load: ${service.next.load}, Feature: ${service.next.feature}, Type: ${service.next.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next: ${nextArrival}, Load: ${service.next.load},${service.next.feature},${service.next.type}</p>`;
             }
 
             if (service.subsequent) {
                 const subsequentArrival = new Date(service.subsequent.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Subsequent Arrival: ${subsequentArrival}, Load: ${service.subsequent.load}, Feature: ${service.subsequent.feature}, Type: ${service.subsequent.type}</p>`;
+                serviceDiv.innerHTML += `<p>Subsequent: ${subsequentArrival}, Load: ${service.subsequent.load},${service.subsequent.feature},${service.subsequent.type}</p>`;
             }
 
             if (service.next2) {
                 const next2Arrival = new Date(service.next2.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next 2 Arrival: ${next2Arrival}, Load: ${service.next2.load}, Feature: ${service.next2.feature}, Type: ${service.next2.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next 2: ${next2Arrival}, Load: ${service.next2.load},${service.next2.feature},${service.next2.type}</p>`;
             }
 
             if (service.next3) {
                 const next3Arrival = new Date(service.next3.time).toLocaleTimeString();
-                serviceDiv.innerHTML += `<p>Next 3 Arrival: ${next3Arrival}, Load: ${service.next3.load}, Feature: ${service.next3.feature}, Type: ${service.next3.type}</p>`;
+                serviceDiv.innerHTML += `<p>Next 3: ${next3Arrival}, Load: ${service.next3.load},${service.next3.feature},${service.next3.type}</p>`;
             }
 
             busTimingsDiv.appendChild(serviceDiv);
