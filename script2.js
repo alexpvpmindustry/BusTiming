fb855aed60710543dd051da29d355f321eb5ceef
apmapmapm <alexpvpmindustry@github.com>
2024-07-25 20:05:39 +0800

    add install button

---
+++ script2.js
@@ -0,0 +1,26 @@
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    document.getElementById('installBtn').style.display = '';
+
+    document.getElementById('installBtn').addEventListener('click', () => {
+        document.getElementById('installBtn').style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            } else {
+                console.log('User dismissed the install prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed successfully', event);
+});
